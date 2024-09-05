ef705e3b58b54f712a27910efe905c8a645f964c
Jeremy Pena <09jeremy09@gmail.com>
2024-09-05 19:24:37 -0400

    Update runtime_function.py

--- Class/runtime_function.py
+++ Class/runtime_function.py
@@ -7,7 +7,7 @@ def runtime(total_hours, down_hours):
     uptime = ((total_hours - down_hours)/total_hours) * 100 #basic percentage math formula
     return round(uptime, 2) #round method takes the first argument and round's it up to the number of decimal places specified in second argument
 
-print(runtime(213, 37))
+print(runtime(213, 37)) #print statement for testing
 
 
 
