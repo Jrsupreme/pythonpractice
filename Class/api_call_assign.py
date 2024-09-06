d4565221d57404d0c6dbb2562d8f168a797ff47a
Ubuntu <ubuntu@ip-172-31-33-71.ec2.internal>
2024-09-06 02:48:54 +0000

    the assignment script

--- Class/api_call_assign.py
+++ Class/api_call_assign.py
@@ -1,26 +1,31 @@
 import requests
-
+  
+  
+  
 print("Hey I got riddles for you!")
-print("I got them by difficulty or random how would you like them: ")
-answer1 = input("type: difficulty or random ")
-if answer1 == random:
+print("Would you like a random riddle or the riddle of the day?")
+answer1 = input("type in 'day' or 'random': ").lower()
+if answer1 == 'random':
     url = "https://riddlie.p.rapidapi.com/api/v1/riddles/random"
-elif answer1 == difficulty:
- difficulty = input("select one of the following difficulties: easy, medium or hard:" 
-    if difficulty = easy:
-        url = "https://riddlie.p.rapidapi.com/api/v1/riddles/bylevel/easy"
-    elif difficulty = medium:
-        url = 
-else: 
-    print("Invalid asnwer")
-querystring = 
-{"flag":"0"}
-{'difficultyLevel': 'medium'}
-headers = {
-	"x-rapidapi-key": "8ccef19d23msh0b552b4b8e8f3d4p1f0d83jsn1dcaa35670f2",
-	"x-rapidapi-host": "riddlie.p.rapidapi.com"
-}
+elif answer1 == 'day':
+    url = "https://riddlie.p.rapidapi.com/api/v1/riddles/rod"
+else:
+     print("Invalid asnwer")
+       
 
-response = requests.get(url, headers=headers, params=querystring)
+querystring: {'flag':'0'}
 
-print(response.json())
+    response = requests.get(url, headers=headers, params=querystring) 
+    response.raise_for_status()  # Will raise an HTTPError for bad responses
+    riddle_data = response.json()
+            
+    # Extract the riddle question and answer
+    riddle_question = riddle_data.get('riddle')
+    riddle_answer = riddle_data.get('answer')
+    print("Sorry, could not retrieve the riddle properly.")
+    
+  
+    headers = {
+        "x-rapidapi-key": "8ccef19d23msh0b552b4b8e8f3d4p1f0d83jsn1dcaa35670f2",
+        "x-rapidapi-host": "riddlie.p.rapidapi.com"
+    }
