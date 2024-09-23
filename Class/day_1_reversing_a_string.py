90a2151168fd38a2605ecc35d6c45578f8763d11
Ubuntu <ubuntu@ip-172-31-33-71.ec2.internal>
2024-09-23 16:23:36 +0000

    day 1

---
+++ Class/day_1_reversing_a_string.py
@@ -0,0 +1,9 @@
+#Reverse a string 
+#prompt user to input a string
+#return said string reversed
+
+user_input = input("Enter the string you want to reverse: ")
+
+reversed_input = user_input[::-1] 
+
+print(reversed_input)
