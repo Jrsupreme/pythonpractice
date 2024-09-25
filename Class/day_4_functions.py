28528fc36c270672b6e47b88e7e2ae1d5fb4565a
Ubuntu <ubuntu@ip-172-31-33-71.ec2.internal>
2024-09-25 18:13:20 +0000

    added comments

--- Class/day_4_functions.py
+++ Class/day_4_functions.py
@@ -4,12 +4,13 @@
 #print result
 # round up the asnwer to .1 decimal place
 
-fahrenheit = float(input("Type in the temperature in fahrenheit: "))
+fahrenheit = float(input("Type in the temperature in fahrenheit: ")) #defining user input as a variable and formating the input to be a float since temperature can have a decimal.
 
 def fahrenheit_to_celcius(fahrenheit):
 
- celcius = (fahrenheit - 32) * 5 / 9 
+ #celcius = (fahrenheit - 32 * 5 / 9)
+ celcius = (fahrenheit - 32) * 5 / 9  #def celcius variable with the formula to convert F to C 
 
- print(f"{fahrenheit} F is {celcius:.1f} C")
+ print(f"{fahrenheit} F is {celcius:.1f} C") #celcius":.1f" to round up answer to one decimal place
 
 fahrenheit_to_celcius(fahrenheit)
