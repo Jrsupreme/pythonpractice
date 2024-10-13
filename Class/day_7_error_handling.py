7710c1bbc087aa70489080920b98521ae400c458
Ubuntu <ubuntu@ip-172-31-33-71.ec2.internal>
2024-10-13 14:33:31 +0000

    day 7

---
+++ Class/day_7_error_handling.py
@@ -0,0 +1,25 @@
+#read file a file
+#print each line of the file with its line number
+import os #need function to check if file exist from this module
+
+try:                                                                            #removed the last "t" from .txt at the end of the file name to test the script
+    file = open("/home/ubuntu/python/python_practice/Class/day_5_sample.tx", "r") #take input to make script re-usable
+    #print(file.read())
+    #check if file path exist
+    line_number = 0 #line count
+    for line in file:
+        line_number +=1
+        print(f"{line_number} {line}")
+
+
+    #for line_count, line in enumerate(file, start=1): #itereate line by line
+    #line_number +=1 #add 1 per line
+    #print(f"{line_number}") #print each line plus a line number
+    #print(f"{line_count} {line}")
+    # file.close() #close the file is good practice to close the file after being done with it.
+    # else:
+    #     print(f"{fp} does not exist") #if file doesn't exist say so
+except:
+    print(f"The file was NOT found. Please check the file's path or name")
+finally:
+    file.close()
